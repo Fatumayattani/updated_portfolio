ef2258994b86cc917171543ca94ee680155d6520
Fatumayattani <fyattani@gmail.com>
2024-11-14 12:46:25 +0300

    save changes

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -6,7 +6,7 @@ const projects = [
   {
     title: 'NFT Marketplace Smart Contract',
     description: 'This smart contract provides a basic NFT (Non-Fungible Token) marketplace on Ethereum. It enables users to create, buy, and manage NFTs.',
-    image: 'https://images.unsplash.com/photo-1731495155373-6f5971b7a369?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    image: 'https://images.unsplash.com/photo-1652858319770-860b97ebe5b2?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     tags: ['Solidity', 'TypeScript', 'Javascript'],
     links: {
       live: 'https://example.com',
