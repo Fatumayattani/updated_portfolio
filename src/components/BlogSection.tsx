81c3b2f9295844a0c19d6844a78843ead4fd6862
Fatumayattani <fyattani@gmail.com>
2024-11-14 13:02:30 +0300

    save changes

--- src/components/BlogSection.tsx
+++ src/components/BlogSection.tsx
@@ -9,7 +9,8 @@ const blogPosts = [
     date: '2024-10-01',
     readTime: '5 min read',
     image: 'https://images.unsplash.com/photo-1642576422624-fd0df2b084d3?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    category: 'Blockchain'
+    category: 'Blockchain',
+    link: '/blog/layer-2-solutions-2024',
   },
   {
     title: 'Mastering React Performance',
